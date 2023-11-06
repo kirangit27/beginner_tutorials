e4b4417e5f0372a0de6144b7c150ccba72653de3
kirangit27 <kiran.suvas.patil@gmail.com>
2023-11-06 13:47:12 -0500

    modified code structure to follow Google C++ Style Guide

---
+++ src/pub_main.cpp
@@ -0,0 +1,15 @@
+
+
+
+
+#include "../include/beginner_tutorials/publisher_member_function.hpp"
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
+
---
+++ src/sub_main.cpp
@@ -0,0 +1,19 @@
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "../include/beginner_tutorials/subscriber_member_function.hpp"
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
+
