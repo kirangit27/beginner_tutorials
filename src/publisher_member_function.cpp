23a74c4c7ad21439a8701e7b8e8b516f77c0219a
kirangit27 <kiran.suvas.patil@gmail.com>
2023-11-06 23:51:11 -0500

    updated custom string message

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -17,7 +17,7 @@
  */
 void MinimalPublisher::timer_callback() {
   auto message = std_msgs::msg::String();
-  message.data = "Hello, world! " + std::to_string(count_++);
+  message.data = "ROS 2 Programming Assignment 1! " + std::to_string(count_++);
   RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
   publisher_->publish(message);
 }
