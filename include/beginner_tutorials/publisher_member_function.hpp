aec5b805f719e8b7c25769eed2b5dbea67fcbf40
kirangit27 <kiran.suvas.patil@gmail.com>
2023-11-19 03:22:00 -0500

    added service to modify string

--- include/beginner_tutorials/publisher_member_function.hpp
+++ include/beginner_tutorials/publisher_member_function.hpp
@@ -15,6 +15,12 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <cstdlib>
+#include <beginner_tutorials/srv/new_msg.hpp>
+
+#include <rclcpp/executors.hpp>
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -27,13 +33,25 @@ class MinimalPublisher : public rclcpp::Node {
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       timer_ = this->create_wall_timer(
           500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      service_ = this->create_service<beginner_tutorials::srv::NewMsg>(
+        "modify_string", [this](
+            const std::shared_ptr<beginner_tutorials::srv::NewMsg::Request> request,
+            std::shared_ptr<beginner_tutorials::srv::NewMsg::Response> response) {
+          modify_string_service(request, response);
+        });
     }
 
   private:
     void timer_callback();
+    void modify_string_service(
+    const std::shared_ptr<beginner_tutorials::srv::NewMsg::Request> request,
+    std::shared_ptr<beginner_tutorials::srv::NewMsg::Response> response);
+    std::string getData();
 
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     size_t count_;
+    std::string base_string_ = "ROS2 Programming Assignment 2";
+    rclcpp::Service<beginner_tutorials::srv::NewMsg>::SharedPtr service_;
 };
 
--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -17,7 +17,39 @@
  */
 void MinimalPublisher::timer_callback() {
   auto message = std_msgs::msg::String();
-  message.data = "ROS 2 Programming Assignment 1! " + std::to_string(count_++);
-  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+
+  message.data = getData();
+  RCLCPP_INFO_STREAM(rclcpp::get_logger("minimal_publisher"),
+                       "Publishing: " << message.data.c_str());
   publisher_->publish(message);
+}
+
+/**
+ * @brief 
+ * 
+ */
+void MinimalPublisher::modify_string_service(
+      const std::shared_ptr<beginner_tutorials::srv::NewMsg::Request> request,
+      std::shared_ptr<beginner_tutorials::srv::NewMsg::Response> response) {
+
+    // RCLCPP_INFO_STREAM(rclcpp::get_logger("minimal_publisher"),
+    //                    "Modifying String - "<<request->new_msg);
+    // RCLCPP_DEBUG_STREAM(rclcpp::get_logger("minimal_publisher"),
+    //                    "Modifying String - "<<request->new_msg);                     
+    // RCLCPP_WARN_STREAM(rclcpp::get_logger("minimal_publisher"),
+    //                    "Modifying String - "<<request->new_msg);  
+    // RCLCPP_ERROR_STREAM(rclcpp::get_logger("minimal_publisher"),
+    //                    "Modifying String - "<<request->new_msg);                     
+    // RCLCPP_FATAL_STREAM(rclcpp::get_logger("minimal_publisher"),
+    //                    "Modifying String - "<<request->new_msg);                                    
+    base_string_ = request->new_msg;
+    response->set__response(true);
+}
+
+/**
+ * @brief 
+ * 
+ */
+std::string MinimalPublisher::getData() {
+    return base_string_ + " - " + std::to_string(count_++);
 }
